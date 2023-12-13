3dd7d6a11f1545b0201b18e8eb6eca2ba0d04790
AnujaSreeni <127194605+AnujaSreeni@users.noreply.github.com>
2023-12-13 10:42:18 +0530

    hai

--- src/Components/student/Studentedit.jsx
+++ src/Components/student/Studentedit.jsx
@@ -1,58 +1,41 @@
+import { Button, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import axios from 'axios'
 import React, { useState } from 'react'
 
 const Studentedit = (props) => {
-    var[inputs,setInputs]=useState(props.data)
-    const inputHandler=(event)=>
-    {
+    var [inputs, setInputs] = useState(props.data)
+    const inputHandler = (event) => {
 
         const { name, value } = event.target
         setInputs((inputs) => ({ ...inputs, [name]: value }))
         console.log(inputs)
     }
-    const addHandler=()=>{
-        if(props.method==="put")
-        {
-            axios.put("http://localhost:3005/edit/"+inputs._id,inputs)
-            .then(response=>{
-                alert("Record updated")
-                window.location.reload(false);
-            })
-            .catch(err=>console.log(err))
+    const addHandler = () => {
+        if (props.method === "put") {
+            axios.put("http://localhost:3005/edit/" + inputs._id, inputs)
+                .then(response => {
+                    alert("Record updated")
+                    window.location.reload(false);
+                })
+                .catch(err => console.log(err))
         }
     }
-  return (
-    <div>
-      <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Admission no</TableCell>
-                            <TableCell >Name</TableCell>
-                            <TableCell >Age</TableCell>
-                            <TableCell >Course</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {student.map((row,pos) => {
-                            return (
+    return (
+        <div>
+            <TextField label="Admission number" name="Admno" variant="outlined" value={inputs.Admno} onChange={inputHandler} /><br /><br />
+        <TextField label="Name" name="Name" variant="outlined" value={inputs.Name} onChange={inputHandler} /><br /><br />
+        <TextField label="Age" name="Age" variant="outlined" value={inputs.Age} onChange={inputHandler} /><br /><br />
+        <InputLabel id="demo-simple-select-label">Course</InputLabel>
+        <Select
+            labelId="demo-simple-select-label" name="Course" value={inputs.Course} onChange={inputHandler}>
+            <MenuItem value={"BCA"}>BCA</MenuItem>npm start
 
-                                <TableRow key={pos}
-
-                                >
-                                    <TableCell >{row.Admno}</TableCell>
-                                    <TableCell >{row.Name}</TableCell>
-                                    <TableCell>{row.Age}</TableCell>
-                                    <TableCell >{row.Course}</TableCell>
-                                   
-                                </TableRow>
-                            )
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-    </div>
-  )
+            <MenuItem value={"BCOM"}>BCOM</MenuItem>
+            <MenuItem value={"BBA"}>BBA</MenuItem>
+        </Select><br /><br />
+        <Button variant="contained"onClick={addHandler}>Submit</Button>
+        </div>
+    )
 }
 
 export default Studentedit
